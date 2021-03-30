a0ab33f458e5cc012a4f67bf78ac363d2f97b242
Nemese Kalubi <nemesee@live.com>
2021-03-29 22:41:32 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import {Security, SecureRoute , LoginCallback } from '@okta/okta-react';
+import {OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'; 
+import { oktaAuthConfig, oktaSignInConfig } from './components/auth/config';
+import { useHistory } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/auth/Login';
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+
+const oktaAuth = new OktaAuth(oktaAuthConfig);
 
 function App() {
+  const history = useHistory();
+
+  const customAuthHandler = (oktaAuth) => {
+    history.push('/login');
+  };
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    try{
+      history.replace(toRelativeUrl(originalUri, 'restoreOriginalUri '));
+    }
+    catch(err){
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+      <Security
+      oktaAuth={oktaAuth}
+      onAuthRequired={customAuthHandler}
+      restoreOriginalUri={restoreOriginalUri}
+    >
+      <Navbar></Navbar>
+      <Switch>
+        <Route path='/' exact={true} component={Home} />
+        <SecureRoute path='/dashboard' component={Dashboard} />
+        <Route path='/login' render={() => <Login config={oktaSignInConfig} />} />
+        <Route path='/login/callback' component={LoginCallback} />
+      </Switch>
+    </Security>
+   </Router>
   );
 }
 
